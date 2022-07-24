840d3d2444f850d114306c9862cef6500f1eb1d6
Mohamed M'hand Ouammi <75146932+kingussopp@users.noreply.github.com>
2022-07-24 03:43:41 +0200

    feat :green_book: : code beautification

--- hnz.cpp
+++ hnz.cpp
@@ -201,28 +201,24 @@ void HNZ::start()
 
     loopActivated = true;
     ////
-   m_ip = m_getConfigValue<string>(m_stack_configuration, "/transport_layer/connections/srv_ip"_json_pointer);
-   m_port = m_getConfigValue<int>(m_stack_configuration, "/transport_layer/connections/port"_json_pointer);
-   // m_ip="192.168.1.3";
-   // m_port=6001;
-
+    m_ip = m_getConfigValue<string>(m_stack_configuration, "/transport_layer/connections/srv_ip"_json_pointer);
+    m_port = m_getConfigValue<int>(m_stack_configuration, "/transport_layer/connections/port"_json_pointer);
     m_retry_number = m_getConfigValue<int>(m_stack_configuration, "/transport_layer/retry_number"_json_pointer);
     m_retry_delay = m_getConfigValue<int>(m_stack_configuration, "/transport_layer/retry_delay"_json_pointer);
-    ////
     m_remote_station_addr= m_getConfigValue<int>(m_stack_configuration, "/application_layer/remote_station_addr"_json_pointer);
     m_local_station_addr= m_getConfigValue<int>(m_stack_configuration, "/application_layer/local_station_addr"_json_pointer);    
-     m_remote_addr_in_local_station= m_getConfigValue<int>(m_stack_configuration, "/application_layer/remote_addr_in_local_station"_json_pointer);
-     m_inacc_timeout= m_getConfigValue<int>(m_stack_configuration, "/application_layer/inacc_timeout"_json_pointer);  
-     m_max_sarm= m_getConfigValue<int>(m_stack_configuration, "/application_layer/max_sarm"_json_pointer);
-     m_to_socket= m_getConfigValue<int>(m_stack_configuration, "/application_layer/to_socket"_json_pointer);
-     m_repeat_path_A= m_getConfigValue<int>(m_stack_configuration, "/application_layer/repeat_path_A"_json_pointer);
-     m_repeat_path_B= m_getConfigValue<int>(m_stack_configuration, "/application_layer/repeat_path_B"_json_pointer);
-     m_repeat_timeout= m_getConfigValue<int>(m_stack_configuration, "/application_layer/repeat_timeout"_json_pointer);
-     m_anticipation= m_getConfigValue<int>(m_stack_configuration, "/application_layer/anticipation"_json_pointer);
-     m_default_msg_period= m_getConfigValue<int>(m_stack_configuration, "/application_layer/default_msg_period"_json_pointer);
-     m_Test_msg_send= m_getConfigValue<string>(m_stack_configuration, "/application_layer/Test_msg_send"_json_pointer);
-     m_Test_msg_receive= m_getConfigValue<string>(m_stack_configuration, "/application_layer/Test_msg_receive"_json_pointer); 
-   Logger::getLogger()->info("Connection initialized");
+    m_remote_addr_in_local_station= m_getConfigValue<int>(m_stack_configuration, "/application_layer/remote_addr_in_local_station"_json_pointer);
+    m_inacc_timeout= m_getConfigValue<int>(m_stack_configuration, "/application_layer/inacc_timeout"_json_pointer);  
+    m_max_sarm= m_getConfigValue<int>(m_stack_configuration, "/application_layer/max_sarm"_json_pointer);
+    m_to_socket= m_getConfigValue<int>(m_stack_configuration, "/application_layer/to_socket"_json_pointer);
+    m_repeat_path_A= m_getConfigValue<int>(m_stack_configuration, "/application_layer/repeat_path_A"_json_pointer);
+    m_repeat_path_B= m_getConfigValue<int>(m_stack_configuration, "/application_layer/repeat_path_B"_json_pointer);
+    m_repeat_timeout= m_getConfigValue<int>(m_stack_configuration, "/application_layer/repeat_timeout"_json_pointer);
+    m_anticipation= m_getConfigValue<int>(m_stack_configuration, "/application_layer/anticipation"_json_pointer);
+    m_default_msg_period= m_getConfigValue<int>(m_stack_configuration, "/application_layer/default_msg_period"_json_pointer);
+    m_Test_msg_send= m_getConfigValue<string>(m_stack_configuration, "/application_layer/Test_msg_send"_json_pointer);
+    m_Test_msg_receive= m_getConfigValue<string>(m_stack_configuration, "/application_layer/Test_msg_receive"_json_pointer); 
+    Logger::getLogger()->info("Connection initialized");
     ////
 
     // Connect to the server

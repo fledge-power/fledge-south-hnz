df5e0deb1a068be8372fa56f7d28a89634ecbf49
Mohamed M'hand Ouammi <75146932+kingussopp@users.noreply.github.com>
2022-07-25 16:18:54 +0200

    Errors correction

--- hnz.cpp
+++ hnz.cpp
@@ -130,19 +130,19 @@ void HNZ::setanticipation(int anticipation){
     setllevel(m_getConfigValue<int>(m_stack_configuration, "/transport_layer/llevel"_json_pointer));
     setretry_number(m_getConfigValue<int>(m_stack_configuration, "/transport_layer/retry_number"_json_pointer));
     setretry_delay(m_getConfigValue<int>(m_stack_configuration, "/transport_layer/retry_delay"_json_pointer));
-    setremote_station_addr(m_getConfigValue<int>(m_stack_configuration, "application_layer/remote_station_addr/"_json_pointer));
-    setlocal_station_addr(m_getConfigValue<int>(m_stack_configuration, "application_layer/local_station_addr/"_json_pointer));
-    setremote_addr_in_local_station(m_getConfigValue<int>(m_stack_configuration, "application_layer/remote_addr_in_local_station/"_json_pointer));
-    setinacc_timeout(m_getConfigValue<int>(m_stack_configuration, "application_layer/inacc_timeout/"_json_pointer));
-    setmax_sarm(m_getConfigValue<int>(m_stack_configuration, "application_layer/max_sarm/"_json_pointer));
-    setto_socket(m_getConfigValue<int>(m_stack_configuration, "application_layer/to_socket/"_json_pointer));
-    setrepeat_path_A(m_getConfigValue<int>(m_stack_configuration, "application_layer/repeat_path_A/"_json_pointer));
-    setrepeat_path_B(m_getConfigValue<int>(m_stack_configuration, "application_layer/repeat_path_B/"_json_pointer));
-    setrepeat_timeout(m_getConfigValue<int>(m_stack_configuration, "application_layer/repeat_timeout/"_json_pointer));
-    setanticipation(m_getConfigValue<int>(m_stack_configuration, "application_layer/anticipation/"_json_pointer));
-    setdefault_msg_period(m_getConfigValue<int>(m_stack_configuration, "application_layer/default_msg_period/"_json_pointer));
-    settest_msg_send(m_getConfigValue<string>(m_stack_configuration, "application_layer/test_msg_send/"_json_pointer));
-    settest_msg_receive(m_getConfigValue<string>(m_stack_configuration, "application_layer/test_msg_receive/"_json_pointer));
+    setremote_station_addr(m_getConfigValue<int>(m_stack_configuration, "/application_layer/remote_station_addr"_json_pointer));
+    setlocal_station_addr(m_getConfigValue<int>(m_stack_configuration, "/application_layer/local_station_addr"_json_pointer));
+    setremote_addr_in_local_station(m_getConfigValue<int>(m_stack_configuration, "/application_layer/remote_addr_in_local_station"_json_pointer));
+    setinacc_timeout(m_getConfigValue<int>(m_stack_configuration, "/application_layer/inacc_timeout"_json_pointer));
+    setmax_sarm(m_getConfigValue<int>(m_stack_configuration, "/application_layer/max_sarm"_json_pointer));
+    setto_socket(m_getConfigValue<int>(m_stack_configuration, "/application_layer/to_socket"_json_pointer));
+    setrepeat_path_A(m_getConfigValue<int>(m_stack_configuration, "/application_layer/repeat_path_A"_json_pointer));
+    setrepeat_path_B(m_getConfigValue<int>(m_stack_configuration, "/application_layer/repeat_path_B"_json_pointer));
+    setrepeat_timeout(m_getConfigValue<int>(m_stack_configuration, "/application_layer/repeat_timeout"_json_pointer));
+    setanticipation(m_getConfigValue<int>(m_stack_configuration, "/application_layer/anticipation"_json_pointer));
+    setdefault_msg_period(m_getConfigValue<int>(m_stack_configuration, "/application_layer/default_msg_period"_json_pointer));
+    settest_msg_send(m_getConfigValue<string>(m_stack_configuration, "/application_layer/test_msg_send"_json_pointer));
+    settest_msg_receive(m_getConfigValue<string>(m_stack_configuration, "/application_layer/test_msg_receive"_json_pointer));
  }
 ////////
 

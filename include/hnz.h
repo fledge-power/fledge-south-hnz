51024bc0b251386a7419cf47854dd6f199006b5a
JustinF18 <justinfacquet@gmail.com>
2022-08-06 11:33:30 +0200

    Beautify (google c++ style) and bugfix

--- include/hnz.h
+++ include/hnz.h
@@ -1,113 +1,120 @@
-//
-// Created by Lucas Barret on 26/05/2021.
-//
+#ifndef HNZ_H
+#define HNZ_H
 
-#ifndef WORK_HNZ_H
-#define WORK_HNZ_H
+/*
+ * Fledge HNZ south plugin.
+ *
+ * Copyright (c) 2022, RTE (https://www.rte-france.com)
+ *
+ * Released under the Apache 2.0 Licence
+ *
+ * Author: Lucas Barret, Colin Constans, Justin Facquet
+ */
 
-#include <iostream>
-#include <cstring>
-#include <reading.h>
 #include <logger.h>
-#include <cstdlib>
-#include <cstdio>
-#include <thread>
-#include <mutex>
+#include <reading.h>
+
 #include <atomic>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <mutex>
+#include <thread>
 #include <utility>
 //#include "../../lib/src/inc/hnz_client.h"
+#include <json.hpp>  // https://github.com/nlohmann/json
+
 #include "../../libhnz/src/inc/hnz_client.h"
-#include <json.hpp> // https://github.com/nlohmann/json
 #include "hnzconf.h"
 
 class HNZFledge;
 
-class HNZ
-{
-public:
-    typedef void (*INGEST_CB)(void *, Reading);
+class HNZ {
+ public:
+  typedef void (*INGEST_CB)(void*, Reading);
 
-    HNZ();
-    ~HNZ() = default;
+  HNZ();
+  ~HNZ() = default;
 
-    void		setAssetName(const std::string& asset) { m_asset = asset; }
-    void		restart();
-    void        start();
-    void		stop();
-    void        receive();
-    int		    connect();
-    void        stop_loop();
-    void        analyze_frame(unsigned char* data, int size);
-    bool        analyze_info_frame(unsigned char *data, unsigned char addr, int ns, int p, int nr, int size);
-    void        sendToFledge(std::string message_type, unsigned char addr, int info_adress, int value, int valid, int ts, 
-                            int ts_iv, int ts_c, int ts_s, std::string label, bool time);
-    std::string convert_data_to_str(unsigned char *data, int len);
+  void setAssetName(const std::string& asset) { m_asset = asset; }
+  void restart();
+  void start();
+  void stop();
+  void receive();
+  int connect();
+  void stop_loop();
+  void analyze_frame(unsigned char* data, int size);
+  bool analyze_info_frame(unsigned char* data, unsigned char addr, int ns,
+                          int p, int nr, int size);
+  void sendToFledge(std::string message_type, unsigned char addr,
+                    int info_adress, int value, int valid, int ts, int ts_iv,
+                    int ts_c, int ts_s, std::string label, bool time);
+  std::string convert_data_to_str(unsigned char* data, int len);
 
-    std::string m_getLabel(const int address,
-                                          const std::string& message_code,
-                                          const int info_address);
+  std::string m_getLabel(const int address, const std::string& message_code,
+                         const int info_address);
 
-    //void ingest(Reading& reading);
-    void ingest(std::string assetName, std::vector<Datapoint *> &points);
-    void registerIngest(void *data, void (*cb)(void *, Reading));
+  // void ingest(Reading& reading);
+  void ingest(std::string assetName, std::vector<Datapoint*>& points);
+  void registerIngest(void* data, void (*cb)(void*, Reading));
 
-    std::string		m_asset;
-    
-    HNZClient*      m_client;
+  std::string m_asset;
 
-    std::mutex loopLock;
-    std::atomic<bool> loopActivated;
-    std::thread loopThread;
-	
-	static void setJsonConfig(const std::string& configuration, const std::string& msg_configuration);
+  HNZClient* m_client;
 
-private:
-    // configuration
-    static HNZConf *m_conf;
+  std::mutex loopLock;
+  std::atomic<bool> loopActivated;
+  std::thread loopThread;
 
-    INGEST_CB			m_ingest;     // Callback function used to send data to south service
-    void*               m_data;       // Ingest function data
-    bool				m_connected;
-    HNZFledge*          m_fledge;
-	int					frame_number, module10M;
-	
-	template <class T>
-    static T m_getConfigValue(nlohmann::json configuration, nlohmann::json_pointer<nlohmann::json> path);
+  void setJsonConfig(const std::string& configuration,
+                     const std::string& msg_configuration);
 
-    static void   m_checkExchangedDataJson(const std::string &msg_configuration);
-	static nlohmann::json m_stack_configuration;
-    static nlohmann::json m_msg_configuration;
+ private:
+  // configuration
+  HNZConf* m_conf;
+
+  INGEST_CB m_ingest;  // Callback function used to send data to south service
+  void* m_data;        // Ingest function data
+  bool m_connected;
+  HNZFledge* m_fledge;
+  int frame_number, module10M;
+
+  template <class T>
+  static T m_getConfigValue(nlohmann::json configuration,
+                            nlohmann::json_pointer<nlohmann::json> path);
+
+  static void m_checkExchangedDataJson(const std::string& msg_configuration);
+  static nlohmann::json m_stack_configuration;
+  static nlohmann::json m_msg_configuration;
 };
 
-class HNZFledge
-{
-public :
-    explicit HNZFledge(HNZ *hnz) : m_hnz(hnz) {};
+class HNZFledge {
+ public:
+  explicit HNZFledge(HNZ* hnz) : m_hnz(hnz){};
 
-    // ==================================================================== //
-    // Note : The overloaded method addData is used to prevent the user from
-    // giving value type that can't be handled. The real work is forwarded
-    // to the private method m_addData
+  // ==================================================================== //
+  // Note : The overloaded method addData is used to prevent the user from
+  // giving value type that can't be handled. The real work is forwarded
+  // to the private method m_addData
 
+  // Sends the datapoints passed as Reading to Fledge
+  void sendData(Datapoint* dp, const std::string& label);
 
-    // Sends the datapoints passed as Reading to Fledge
-    void sendData(Datapoint* dp, const std::string& label);
+  template <class T>
+  Datapoint* m_addData(std::string message_type, unsigned char addr,
+                       int info_adress, int value, int valid, int ts, int ts_iv,
+                       int ts_c, int ts_s, bool time);
 
-    template<class T>
-    Datapoint* m_addData(std::string message_type, unsigned char addr, int info_adress,
-               int value, int valid, int ts, int ts_iv, int ts_c, int ts_s, bool time);
-private:
+ private:
+  template <class T>
+  static Datapoint* m_createDatapoint(const std::string& dataname,
+                                      const T value) {
+    DatapointValue dp_value = DatapointValue(value);
+    return new Datapoint(dataname, dp_value);
+  }
 
-    template<class T>
-    static Datapoint* m_createDatapoint(const std::string& dataname, const T value)
-    {
-        DatapointValue dp_value = DatapointValue(value);
-        return new Datapoint(dataname,dp_value);
-    }
-
-    HNZ* m_hnz;
-	
+  HNZ* m_hnz;
 };
 
-
-#endif //WORK_HNZ_H
+#endif

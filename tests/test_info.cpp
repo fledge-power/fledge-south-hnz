38c948b77293b66f6614833d10be9ad1e87b8d66
Lucas-Barret <lucas.barret.pro@gmail.com>
2022-09-07 18:28:08 +0200

    Adding test

---
+++ tests/test_info.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+#include <plugin_api.h>
+#include <rapidjson/document.h>
+#include <string.h>
+
+#include <string>
+
+using namespace std;
+using namespace rapidjson;
+
+extern "C"
+{
+    PLUGIN_INFORMATION *plugin_info();
+};
+
+TEST(HNZ, PluginInfo)
+{
+    PLUGIN_INFORMATION *info = plugin_info();
+    ASSERT_STREQ(info->name, "hnz");
+    ASSERT_EQ(info->type, PLUGIN_TYPE_SOUTH);
+}
+
+TEST(HNZ, PluginInfoConfigParse)
+{
+    PLUGIN_INFORMATION *info = plugin_info();
+    Document doc;
+    doc.Parse(info->config);
+    ASSERT_EQ(doc.HasParseError(), false);
+    ASSERT_EQ(doc.IsObject(), true);
+    ASSERT_EQ(doc.HasMember("plugin"), true);
+}

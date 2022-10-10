62e4a84ae4ed810cac01a7ec24f85b617cb6c0a7
JustinF18 <75577798+JustinF18@users.noreply.github.com>
2022-10-10 21:25:03 +0200

    Modification of the tests on the configuration

--- tests/test_hnzconf.cpp
+++ tests/test_hnzconf.cpp
@@ -22,18 +22,18 @@ string protocol_stack_def = QUOTE({
     },
     "application_layer" : {
       "remote_station_addr" : 12,
-      "inacc_timeout" : 180,
-      "max_sarm" : 30,
-      "repeat_path_A" : 3,
-      "repeat_path_B" : 3,
-      "repeat_timeout" : 3000,
-      "anticipation" : 3,
-      "test_msg_send" : "1304",
-      "test_msg_receive" : "1304",
-      "gi_schedule" : "99:99",
-      "gi_repeat_count" : 3,
-      "gi_time" : 255,
-      "c_ack_time" : 10
+      "inacc_timeout" : 200,
+      "max_sarm" : 40,
+      "repeat_path_A" : 5,
+      "repeat_path_B" : 2,
+      "repeat_timeout" : 2000,
+      "anticipation_ratio" : 5,
+      "test_msg_send" : "1305",
+      "test_msg_receive" : "1306",
+      "gi_schedule" : "18:05",
+      "gi_repeat_count" : 5,
+      "gi_time" : 300,
+      "c_ack_time" : 20
     }
   }
 });
@@ -55,7 +55,7 @@ string exchanged_data_def = QUOTE({
           },
           {
             "name" : "hnz",
-            "station_address" : 1,
+            "station_address" : 12,
             "message_address" : 511,
             "message_code" : "TSCE"
           }
@@ -130,43 +130,102 @@ TEST_F(HNZConfTest, GetRemoteStationAddr) {
 }
 
 TEST_F(HNZConfTest, GetInaccTimeout) {
-  ASSERT_EQ(hnz_conf->get_inacc_timeout(), 180);
+  ASSERT_EQ(hnz_conf->get_inacc_timeout(), 200);
 }
 
-TEST_F(HNZConfTest, GetMaxSARM) { ASSERT_EQ(hnz_conf->get_max_sarm(), 30); }
+TEST_F(HNZConfTest, GetMaxSARM) { ASSERT_EQ(hnz_conf->get_max_sarm(), 40); }
 
 TEST_F(HNZConfTest, GetRepeatPathAB) {
-  ASSERT_EQ(hnz_conf->get_repeat_path_A(), 3);
-  ASSERT_EQ(hnz_conf->get_repeat_path_B(), 3);
+  ASSERT_EQ(hnz_conf->get_repeat_path_A(), 5);
+  ASSERT_EQ(hnz_conf->get_repeat_path_B(), 2);
 }
 
 TEST_F(HNZConfTest, GetRepeatTimeout) {
-  ASSERT_EQ(hnz_conf->get_repeat_timeout(), 3000);
+  ASSERT_EQ(hnz_conf->get_repeat_timeout(), 2000);
 }
 
 TEST_F(HNZConfTest, GetAnticipationRatio) {
-  ASSERT_EQ(hnz_conf->get_anticipation_ratio(), 3);
+  ASSERT_EQ(hnz_conf->get_anticipation_ratio(), 5);
 }
 
 TEST_F(HNZConfTest, GetTestMsgSend) {
   ASSERT_EQ(hnz_conf->get_test_msg_send().first, 0x13);
-  ASSERT_EQ(hnz_conf->get_test_msg_send().second, 0x04);
+  ASSERT_EQ(hnz_conf->get_test_msg_send().second, 0x05);
 }
 
 TEST_F(HNZConfTest, GetTestMsgReceive) {
   ASSERT_EQ(hnz_conf->get_test_msg_receive().first, 0x13);
-  ASSERT_EQ(hnz_conf->get_test_msg_receive().second, 0x04);
+  ASSERT_EQ(hnz_conf->get_test_msg_receive().second, 0x06);
 }
 
 TEST_F(HNZConfTest, GetGISchedule) {
-  // TODO : Add other case
-  ASSERT_FALSE(hnz_conf->get_gi_schedule().activate);
+  ASSERT_TRUE(hnz_conf->get_gi_schedule().activate);
+  ASSERT_EQ(hnz_conf->get_gi_schedule().hour, 18);
+  ASSERT_EQ(hnz_conf->get_gi_schedule().min, 05);
 }
 
 TEST_F(HNZConfTest, GetGIRepeatCount) {
-  ASSERT_EQ(hnz_conf->get_gi_repeat_count(), 3);
+  ASSERT_EQ(hnz_conf->get_gi_repeat_count(), 5);
 }
 
-TEST_F(HNZConfTest, GetGITime) { ASSERT_EQ(hnz_conf->get_gi_time(), 255); }
+TEST_F(HNZConfTest, GetGITime) { ASSERT_EQ(hnz_conf->get_gi_time(), 300); }
 
-TEST_F(HNZConfTest, GetCAckTime) { ASSERT_EQ(hnz_conf->get_c_ack_time(), 10); }
+TEST_F(HNZConfTest, GetCAckTime) { ASSERT_EQ(hnz_conf->get_c_ack_time(), 20); }
+
+TEST_F(HNZConfTest, GetLabelTS1) {
+  ASSERT_STREQ(hnz_conf->getLabel("TSCE", 511).c_str(), "TS1");
+}
+
+TEST_F(HNZConfTest, GetLabelUnknown) {
+  ASSERT_STREQ(hnz_conf->getLabel("TSCE", 999).c_str(), "");
+}
+
+string min_protocol_stack_def = QUOTE({
+  "protocol_stack" : {
+    "name" : "hnzclient",
+    "version" : "1.0",
+    "transport_layer" : {"connections" : [ {"srv_ip" : "0.0.0.0"} ]},
+    "application_layer" : {"remote_station_addr" : 18}
+  }
+});
+
+TEST(HNZCONF, MinimumConf) {
+  HNZConf* hnz_conf = new HNZConf();
+  hnz_conf->importConfigJson(min_protocol_stack_def);
+  hnz_conf->importExchangedDataJson(exchanged_data_def);
+
+  ASSERT_TRUE(hnz_conf->is_complete());
+
+  ASSERT_STREQ(hnz_conf->get_ip_address().c_str(), "0.0.0.0");
+
+  ASSERT_EQ(hnz_conf->get_port(), 6001);
+
+  ASSERT_EQ(hnz_conf->get_remote_station_addr(), 18);
+
+  ASSERT_EQ(hnz_conf->get_inacc_timeout(), 180);
+
+  ASSERT_EQ(hnz_conf->get_max_sarm(), 30);
+
+  ASSERT_EQ(hnz_conf->get_repeat_path_A(), 3);
+  ASSERT_EQ(hnz_conf->get_repeat_path_B(), 3);
+
+  ASSERT_EQ(hnz_conf->get_repeat_timeout(), 3000);
+
+  ASSERT_EQ(hnz_conf->get_anticipation_ratio(), 3);
+
+  ASSERT_EQ(hnz_conf->get_test_msg_send().first, 0x13);
+  ASSERT_EQ(hnz_conf->get_test_msg_send().second, 0x04);
+
+  ASSERT_EQ(hnz_conf->get_test_msg_receive().first, 0x13);
+  ASSERT_EQ(hnz_conf->get_test_msg_receive().second, 0x04);
+
+  ASSERT_FALSE(hnz_conf->get_gi_schedule().activate);
+
+  ASSERT_EQ(hnz_conf->get_gi_repeat_count(), 3);
+
+  ASSERT_EQ(hnz_conf->get_gi_time(), 255);
+
+  ASSERT_EQ(hnz_conf->get_c_ack_time(), 10);
+
+  delete hnz_conf;
+}

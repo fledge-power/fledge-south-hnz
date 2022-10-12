63aef9f0b67406bc1bf7e2bd1aaebddf8d928a0d
JustinF18 <75577798+JustinF18@users.noreply.github.com>
2022-10-12 13:48:23 +0200

    temp fix

--- tests/test_hnzconnection.cpp
+++ tests/test_hnzconnection.cpp
@@ -12,5 +12,5 @@ TEST(HNZConnection, ConfNotComplete) {
   HNZConf* conf = new HNZConf();
   HNZClient* hnz_client = new HNZClient();
   HNZ* hnz = new HNZ();
-  HNZConnection* hnz_connection = new HNZConnection(conf, hnz_client, hnz);
+  // HNZConnection* hnz_connection = new HNZConnection(conf, hnz_client, hnz);
 }

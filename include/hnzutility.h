3d7bbf02517b960186c5f15b3b9272793cc68219
Florent Peyrusse <florent.peyrusse@external.equans.com>
2024-06-26 14:13:18 +0200

    refs #PMP2-244 Added audit events when connection is etablished or lost.

--- include/hnzutility.h
+++ include/hnzutility.h
@@ -14,6 +14,7 @@
 
 #include <string>
 #include <logger.h>
+#include <audit_logger.h>
 
 #define PLUGIN_NAME "hnz"
 
@@ -67,6 +68,22 @@ namespace HnzUtility {
         fflush(stdout);
         #endif
         Logger::getLogger()->fatal(format.c_str(), std::forward<Args>(args)...);
+    }
+
+    inline void audit_fail(const std::string& code, const std::string& data) {  
+        AuditLogger::getLogger()->auditLog(code.c_str(), "FAILURE", data.c_str());
+    }
+
+    inline void audit_success(const std::string& code, const std::string& data) {  
+        AuditLogger::getLogger()->auditLog(code.c_str(), "SUCCESS", data.c_str());
+    }
+
+    inline void audit_warn(const std::string& code, const std::string& data) {  
+        AuditLogger::getLogger()->auditLog(code.c_str(), "WARNING", data.c_str());
+    }
+
+    inline void audit_info(const std::string& code, const std::string& data) {  
+        AuditLogger::getLogger()->auditLog(code.c_str(), "INFORMATION", data.c_str());
     }
 }
 
--- tests/test_utility.cpp
+++ tests/test_utility.cpp
@@ -11,3 +11,12 @@ TEST(HNZUtility, Logs)
     ASSERT_NO_THROW(HnzUtility::log_error(text.c_str(), "error"));
     ASSERT_NO_THROW(HnzUtility::log_fatal(text.c_str(), "fatal"));
 }
+
+TEST(HNZUtility, Audit)
+{
+    std::string text("This audit is of type ");
+    ASSERT_NO_THROW(HnzUtility::audit_success("SRVFL", text + "success"));
+    ASSERT_NO_THROW(HnzUtility::audit_info("SRVFL", text + "info"));
+    ASSERT_NO_THROW(HnzUtility::audit_warn("SRVFL", text + "warn"));
+    ASSERT_NO_THROW(HnzUtility::audit_fail("SRVFL", text + "fail"));
+}
